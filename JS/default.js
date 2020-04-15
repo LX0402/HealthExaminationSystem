bbdb82836a19b8bc2b4d94ccfef615481f451a67
TataJiang <Tatajiang136137@gmail.com>
2020-04-15 13:37:10 +0800

    修改部分内容
    修改文件路径为相对路径
    修改服务部署位置

--- JS/default.js
+++ JS/default.js
@@ -1,7 +1,7 @@
 //用于本地测试
-//var myUrl = "http://localhost:8080/medical_test_war_exploded";
+//var myUrl = "http://localhost:8080/medical_test_war";
 //用于服务器测试
-var myUrl = "http://139.9.204.172";
+var myUrl = "http://39.107.15.76/medical-test";
 
 
 //登录
